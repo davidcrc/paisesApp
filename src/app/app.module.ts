fa4f9d86868aeefb2cd3c8a033e320105f9bd6a8
davidcrc <david_drcr@hotmail.com>
2021-12-08 17:28:48 -0500

    8-8. RouterModule - Rutas en nuestra aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PaisModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
