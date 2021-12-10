c5486bcd5899444e900a2846e52a6baf85daafda
davidcrc <david_drcr@hotmail.com>
2021-12-10 10:55:45 -0500

    8-19. Por Capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,12 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
   styles: [],
 })
-export class PorCapitalComponent implements OnInit {
-  constructor() {}
+export class PorCapitalComponent {
+  termino: string = '';
+  hayError: Boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {}
+  constructor(private paisService: PaisService) {}
+
+  Buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    console.log('buscate', this.termino);
+
+    this.paisService.buscarCapital(this.termino).subscribe(
+      (paises) => {
+        console.log('paises', paises);
+        this.paises = paises;
+      },
+      (err) => {
+        console.log('err', err);
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    // TODO: crear sugernecias
+  }
 }
